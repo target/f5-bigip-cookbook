b3a5d0c2d928fe348ebb5ceb998c339042b68cda
Sergio Rua <sergio.rua@skybettingandgaming.com>
2016-02-05 14:41:10 +0000

    updated metadata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,13 @@
+# This version is a fork of
+# https://github.com/target/f5-bigip-cookbook/
+# and it's not maintained by Target.
+#
+# With my compliments to Target and Jacob, this version supports iRules and Partitions whilst the original
+# version does not and probably never will.
+#
 name             'f5-bigip'
-maintainer       'Target'
-maintainer_email 'jacob.mccann2@target.com'
+maintainer       'Sergio Rua'
+maintainer_email 'github@rua.me.uk'
 license          'Apache 2.0'
 description      'Installs/Configures f5-icontrol'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
