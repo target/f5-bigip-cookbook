bf2c0056c8cef7b87465cf93f7a08151fd457d61
Jacob McCann <jacob.mccann2@target.com>
2016-03-17 08:41:58 -0500

    Version 0.5.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@ f5-bigip Cookbook CHANGELOG
 ==============================
 This file is used to list changes made in each version of the f5-bigip cookbook.
 
+v0.5.2
+------
+* Remove subdirs in libraries folder (#9)
+
 v0.5.1
 ------
 * Bugfix: Load System.Inet interface (#7)
--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'jacob.mccann2@target.com'
 license          'Apache 2.0'
 description      'Installs/Configures f5-icontrol'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.5.1'
+version          '0.5.2'
 
 depends 'chef-vault'
