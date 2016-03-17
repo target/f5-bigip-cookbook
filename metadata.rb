ea66014af345123324b2768bdcba236acc3c18cc
Jacob McCann <jacob.mccann2@target.com>
2016-03-17 10:49:53 -0500

    Add issues and source url to metadata

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,8 @@ maintainer_email 'jacob.mccann2@target.com'
 license          'Apache 2.0'
 description      'Installs/Configures f5-icontrol'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+source_url       'https://github.com/target/f5-bigip-cookbook'
+issues_url       'https://github.com/target/f5-bigip-cookbook/issues'
 version          '0.5.2'
 
 depends 'chef-vault'
