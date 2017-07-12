97fabbe6d3935004fe7d0fd9ce89a452fbf6be24
Tim Smith <tsmith@chef.io>
2017-07-12 13:26:34 -0700

    Use a SPDX compliant license string in the metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'f5-bigip'
 maintainer       'Target'
 maintainer_email 'jacob.mccann2@target.com'
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'Installs/Configures f5-icontrol'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url       'https://github.com/target/f5-bigip-cookbook'
